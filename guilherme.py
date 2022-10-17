80c39803e564c9a3ac948d0bf4cc87e8b1b9165d
guilhermehmt <lucasg354a@gmail.com>
2022-10-17 15:18:15 -0300

    scripty da idade

---
+++ guilherme.py
@@ -0,0 +1,13 @@
+idade = int(input("digite sua idade:"))
+
+if idade < 12: 
+    print("voce é criança")
+    
+elif idade < 18: 
+    print("voce é adolecente")
+    
+elif idade < 60:
+    print("voce é adulto")
+
+else:
+    print("voce é idoso")
